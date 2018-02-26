539a966d35c611e3ea8d16ef41b900ebd30b3fe2
NikhileshChaudhary2903 <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2018-02-25 18:42:27 -0800

    Delete Node in a Linked List

---
+++ Linked List/Easy/deleteNode.c
@@ -0,0 +1,19 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+void deleteNode(struct ListNode* node) {
+   
+    struct ListNode* ptr=node->next;
+    node->val=ptr->val;
+    node->next=ptr->next;
+    ptr->next=NULL;
+    
+    free(ptr);
+    
+
+    
+}
