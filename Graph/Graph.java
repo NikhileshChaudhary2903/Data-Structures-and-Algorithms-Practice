e5ce9febbe3c38f1fb538821b0d8dc7b969ccc01
NikhileshChaudhary2903 <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2019-03-01 14:01:01 -0800

    Update Graph.java

--- Graph/Graph.java
+++ Graph/Graph.java
@@ -74,6 +74,39 @@ void bfs(int i) {
 	}
 	
 	
+	void dfs(int i) {
+		
+		// visited set
+		Set<Integer> set =new HashSet<>();
+		Stack<Integer> stack =new Stack<>();
+		
+		stack.push(i);
+		
+		while(!stack.isEmpty())
+		{
+		 int top = stack.pop();	
+		 
+		 if(!set.contains(top))
+		 {
+			 set.add(top);
+			 System.out.println(top);
+		 }
+		
+			 for(int u:this.adjList[top])
+				{
+					if(!set.contains(u))
+					{	
+					stack.push(u);
+					}
+				} 
+			 
+		 }
+		 
+		}
+		
+		
+		
+	}
 	
 	
 }
