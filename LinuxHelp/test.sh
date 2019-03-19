9d9d7f5580ba9626d820ef58ac4963bd78025139
NikhileshChaudhary2903 <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2019-03-18 17:37:33 -0700

    Run checks on numbers

---
+++ LinuxHelp/test.sh
@@ -0,0 +1,11 @@
+echo "enter number"
+read num
+if [ $num -lt 10 ]
+then
+     echo " that was under the belt"
+elif [ $num -gt 20 ]
+then
+     echo "went over my head"
+else
+     echo "makes sense now"
+fi
