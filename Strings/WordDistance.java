7933eb665c663257239faae9b83d5d7bef16995f
Nikhilesh Chaudhary (Nick) <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2019-06-08 15:55:48 -0700

    Update WordDistance.java

--- Strings/WordDistance.java
+++ Strings/WordDistance.java
@@ -43,4 +43,27 @@ public class WordDistance {
         return result;
 
     }
+     public static int shortestDistance(String[] words, String word1, String word2) {
+
+        int m = -1,n = -1,min = Integer.MAX_VALUE;
+
+        for(int i = 0;i<words.length;i++) {
+
+            if(word1.equals(words[i]))
+            {
+                m = i;
+
+                if(n!=-1)
+                  min = Math.min(min,m-n);
+
+            } else if(word2.equals(words[i])) {
+
+                n = i;
+
+                if(m!=-1)
+                    min = Math.min(min,n-m);
+            }
+        }
+            return min;
+    }
 }
