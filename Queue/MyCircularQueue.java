8ad64c4863b3d708be1b78325d5c8c6786b2afa1
Nikhilesh Chaudhary (Nick) <33855202+NikhileshChaudhary2903@users.noreply.github.com>
2019-06-26 15:15:02 -0700

    Create MyCircularQueue.java

---
+++ Queue/MyCircularQueue.java
@@ -0,0 +1,64 @@
+public class MyCircularQueue {
+    /** Initialize your data structure here. Set the size of the queue to be k. */
+
+    private int[] items;
+    private int front,rear,size;
+
+    public MyCircularQueue(int k) {
+
+        items = new int[k];
+        front = 0;
+        rear = -1; // next empty slot to be added
+        size = 0;
+    }
+
+    /** Insert an element into the circular queue. Return true if the operation is successful. */
+    public boolean enQueue(int value) {
+
+        if(isFull()) {
+            return false;
+        }
+        rear = (rear + 1)% items.length;
+        items[rear] = value;
+        size++;
+        return true;
+    }
+
+    /** Delete an element from the circular queue. Return true if the operation is successful. */
+    public boolean deQueue() {
+
+        if(isEmpty())
+            return false;
+
+        front = (front + 1) % this.items.length;
+        size--;
+        return true;
+    }
+
+    /** Get the front item from the queue. */
+    public int Front() {
+        return (isEmpty())? -1: items[front];
+    }
+
+    /** Get the last item from the queue. */
+    public int Rear() {
+
+        return (isEmpty())? -1: items[rear];
+
+    }
+
+    /** Checks whether the circular queue is empty or not. */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /** Checks whether the circular queue is full or not. */
+    public boolean isFull() {
+        return size == this.items.length;
+    }
+
+    public String toString(){
+        return Arrays.toString(items);
+    }
+
+}
